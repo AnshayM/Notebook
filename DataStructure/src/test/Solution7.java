0fae22e6ff6226371c486013f97be467e90e3713
Anshay <anshaym@163.com>
2019-04-21 12:39:02 +0800

    update the structure

---
+++ DataStructure/src/test/Solution7.java
@@ -0,0 +1,8 @@
+package test;
+
+/**
+ * @author: Anshay
+ * @date: 2019/4/17
+ */
+public class Solution7 {
+}
---
+++ DataStructure/src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @author: Anshay
+ * @date: 2019/4/19
+ */
+public class test {
+    public static void main(String[] args) {
+        String str = "12345";
+        int i = 0;
+        char a = str.charAt(++i);
+        System.out.print(a);
+
+    }
+}
