834a42df4b660f23ce07d3354348a67ed37dbc65
Anshay <anshayM@163.com>
2019-04-22 09:13:27 +0800

    新增工具类

---
+++ DataStructure/src/util/MyUtil.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.logging.Logger;
+
+/**
+ * 工具类
+ *
+ * @author: Anshay
+ * @date: 2019/4/22
+ */
+public class MyUtil {
+    /*Log*/
+    public static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+}
