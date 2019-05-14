e051df732a5b8f67fa0b6b162681e6a6d9ecaa25
anshay <anshay@foxmail.com>
2019-05-14 22:27:03 +0800

    0514_home

---
+++ DataStructure/src/array/Solution10.java
@@ -0,0 +1,49 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: Anshay
+ * @date: 2019/5/14
+ */
+public class Solution10 {
+    public static void main(String[] args) {
+        generate(5);
+        int a = 0;
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        if (numRows <= 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> resList = new ArrayList<>();
+        List<Integer> subList;
+        List<Integer> preList;
+        /*1层*/
+        resList.add(Arrays.asList(new Integer[]{1}));
+        if (numRows == 1) {
+            return resList;
+        }
+        /*2层*/
+        subList = new ArrayList<>();
+        subList.add(1);
+        subList.add(1);
+        resList.add(Arrays.asList(new Integer[]{1, 1}));
+        if (numRows == 2) {
+            return resList;
+        }
+        for (int i = 2; i < numRows; i++) {
+            subList = new ArrayList<>();
+            preList = resList.get(i - 1);
+            subList.add(1);
+            for (int j = 0; j < preList.size() - 1; j++) {
+                subList.add(preList.get(j) + preList.get(j + 1));
+            }
+            subList.add(1);
+            resList.add(subList);
+        }
+        return resList;
+    }
+}
---
+++ DataStructure/src/array/Solution9.java
@@ -0,0 +1,33 @@
+package array;
+
+import util.MyUtil;
+
+/**
+ * 最大连续1的个数
+ * 输入的数组只包含 0 和1。
+ * 输入数组的长度是正整数，且不超过 10,000。
+ *
+ * @author: Anshay
+ * @date: 2019/5/14
+ */
+public class Solution9 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 0, 1, 1, 1};
+        int res = findMaxConsecutiveOnes(nums);
+        MyUtil.print(res);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                res = res >= count ? res : count;
+                count = 0;
+            }
+        }
+        return Math.max(res, count);
+    }
+}
