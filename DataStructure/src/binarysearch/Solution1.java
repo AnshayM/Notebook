b6d8ee1efbd6d8cada15b82b7817da1f9d54302d
Anshay <anshayM@163.com>
2019-05-29 17:16:01 +0800

    0529

---
+++ DataStructure/src/binarysearch/Solution1.java
@@ -0,0 +1,34 @@
+package binarysearch;
+
+/**
+ * 二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，
+ * 如果目标值存在返回下标，否则返回 -1。
+ * <p>
+ * 注意：循环时，要的时mid+1
+ *
+ * @author: Anshay
+ * @date: 2019/5/28
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        int a = search(nums, 9);
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
