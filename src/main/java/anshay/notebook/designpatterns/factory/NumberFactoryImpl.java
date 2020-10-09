f83e310490f5473136abd70a711686d9b83105e3
machao <anshaym@163.com>
2020-10-09 09:24:25 +0800

    工厂模式test

---
+++ src/main/java/anshay/notebook/designpatterns/factory/NumberFactoryImpl.java
@@ -0,0 +1,14 @@
+package anshay.notebook.designpatterns.factory;
+
+import java.math.BigDecimal;
+
+/**
+ * @author machao
+ * @date 2020/9/30
+ */
+public class NumberFactoryImpl implements NumberFactory {
+
+    public Number parse(String s) {
+        return new BigDecimal(s);
+    }
+}
