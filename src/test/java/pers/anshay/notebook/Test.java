8528b17395507496e6d6a8a44632985c00b841d0
machao <anshaym@163.com>
2020-10-20 10:34:30 +0800

    重排链表

--- src/test/java/pers/anshay/notebook/Test.java
+++ src/test/java/pers/anshay/notebook/Test.java
@@ -1,5 +1,7 @@
 package pers.anshay.notebook;
 
+import anshay.notebook.util.ListNode;
+
 /**
  * @author machao
  * @date 2020/10/19
@@ -15,5 +17,19 @@ public class Test {
         }
         //这里是下角标
         return course + 1;
+    }
+
+    /**
+     * 重排链表
+     *
+     * @param head
+     */
+    public void reorderList(ListNode head) {
+        ListNode beforeHead = new ListNode(-1, head);
+        //思路，分别用快慢指针，遍历一遍找到中间的。用栈录入慢节点
+        //以中间为标准点，退栈和往后遍历，做插入操作
+        ListNode first = beforeHead;
+        ListNode slow = beforeHead;
+
     }
 }
