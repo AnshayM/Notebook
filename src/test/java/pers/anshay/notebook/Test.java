681337d3282deb3b2acd5f91df98f4cfa128aa79
anshay <anshay@foxmail.com>
2020-10-26 09:55:23 +0800

    数组中最长的山脉

--- src/test/java/pers/anshay/notebook/Test.java
+++ src/test/java/pers/anshay/notebook/Test.java
@@ -1,6 +1,6 @@
 package pers.anshay.notebook;
 
-import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * @author machao
@@ -8,17 +8,9 @@ import java.util.Arrays;
  */
 public class Test {
     public int videoStitching(int[][] clips, int T) {
-        int[] dp = new int[T + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE - 1);
-        dp[0] = 0;
-        for (int i = 1; i <= T; i++) {
-            for (int[] clip : clips) {
-                //表示这个片段刚好是这个的开头（从0秒开始必须满足，不然后面就没了）
-                if (clip[0] < i && i <= clip[1]) {
-                    dp[i] = Math.min(dp[i], dp[clip[0]] + 1);
-                }
-            }
-        }
-        return dp[T] == Integer.MAX_VALUE - 1 ? -1 : dp[T];
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(8);
+        return 0;
     }
+
 }
