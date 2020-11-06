056818a7aac77f32ff7923a4beb08250a97c825b
machao <anshaym@163.com>
2020-11-06 23:06:51 +0800

    根据数字二进制下 1 的数目排序

--- src/test/java/pers/anshay/notebook/Test.java
+++ src/test/java/pers/anshay/notebook/Test.java
@@ -1,14 +1,27 @@
 package pers.anshay.notebook;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author machao
  * @date 2020/10/19
  */
 public class Test {
+    public int[] sortByBits(int[] arr) {
+        int[] map = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            map[i] = Integer.bitCount(arr[i]) * 10000000 + arr[i];
+        }
+        Arrays.sort(map);
+        for (int i = 0; i < map.length; i++) {
+            map[i] = map[i] % 10000000;
+        }
+        return map;
+    }
 
-
-
+    public static void main(String[] args) {
+        ConcurrentHashMap hashMap = new ConcurrentHashMap();
+    }
 
 }
