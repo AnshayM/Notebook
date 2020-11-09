44a7bcc36bf80f542661b6206f5004ebb54777b8
machao <anshaym@163.com>
2020-11-09 18:02:42 +0800

    973. 最接近原点的 K 个点

--- src/main/java/anshay/notebook/leetcode/solvd/Solution973.java
+++ src/main/java/anshay/notebook/leetcode/solvd/Solution973.java
@@ -1,22 +1,16 @@
-package pers.anshay.notebook;
+package anshay.notebook.leetcode.solvd;
 
 import java.util.Arrays;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * 973. 最接近原点的 K 个点
+ *
  * @author machao
- * @date 2020/10/19
+ * @date 2020/11/9
  */
-public class Test {
+public class Solution973 {
     public int[][] kClosest(int[][] points, int K) {
-
-
         Arrays.sort(points, (v1, v2) -> v1[0] * v1[0] - v2[0] * v2[0] + v1[1] * v1[1] - v2[1] * v2[1]);
         return Arrays.copyOf(points, K);
     }
-
-    public static void main(String[] args) {
-        ConcurrentHashMap hashMap = new ConcurrentHashMap();
-    }
-
 }
