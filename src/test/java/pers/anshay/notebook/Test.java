872949974c90f7827f393864047d412f5070c115
anshay <anshay@foxmail.com>
2020-11-13 17:19:24 +0800

    奇偶链表

--- src/test/java/pers/anshay/notebook/Test.java
+++ src/test/java/pers/anshay/notebook/Test.java
@@ -1,5 +1,7 @@
 package pers.anshay.notebook;
 
+import anshay.notebook.common.pojo.ListNode;
+
 import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,6 +19,42 @@ public class Test {
 
     public static void main(String[] args) {
         ConcurrentHashMap hashMap = new ConcurrentHashMap();
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = new ListNode(-1, head);
+        ListNode node1 = head;
+        ListNode node2 = head.next;
+        ListNode pre2 = new ListNode(-1, node2);
+        while (node1.next != null && node2.next != null) {
+            node1.next = node2.next;
+            node1 = node1.next;
+            node2.next = node1.next;
+            node2 = node2.next;
+        }
+        node1.next = pre2.next;
+        return pre.next;
+    }
+
+    public static ListNode oddEvenList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = new ListNode(-1, head);//奇数pre
+        ListNode node1 = head;//奇数当前
+        ListNode node2 = head.next;//偶数当前
+        ListNode pre2 = new ListNode(-1, node2);//偶数pre
+        while (node1.next != null && node2.next != null) {
+            node1.next = node2.next;//隔一个偶后就是下一个奇
+            node1 = node1.next;//移动指针
+            node2.next = node1.next;//已经挪到一下的奇的后一个就是下一个偶
+            node2 = node2.next;//移动指针
+        }
+        node1.next = pre2.next;
+        return pre.next;
     }
 
 }
