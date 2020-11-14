d83eb172b1c490d9c362c62d93ab8148f15815c1
anshay <anshay@foxmail.com>
2020-11-14 10:38:13 +0800

    1122. 数组的相对排序

--- src/test/java/pers/anshay/notebook/Test.java
+++ src/test/java/pers/anshay/notebook/Test.java
@@ -17,44 +17,24 @@ public class Test {
         return Arrays.copyOf(points, K);
     }
 
-    public static void main(String[] args) {
-        ConcurrentHashMap hashMap = new ConcurrentHashMap();
-    }
-
-    public static ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        //第一步：将arr1中数据分配到各桶中
+        int[] bucket = new int[1001];
+        int[] res = new int[arr1.length];
+        for (int data : arr1)
+            bucket[data]++;
+        //第二步：按照arr2的顺序收集桶中数据
+        int j = 0;
+        for (int data : arr2) {
+            while (bucket[data]-- > 0)
+                res[j++] = data;
         }
-        ListNode pre = new ListNode(-1, head);
-        ListNode node1 = head;
-        ListNode node2 = head.next;
-        ListNode pre2 = new ListNode(-1, node2);
-        while (node1.next != null && node2.next != null) {
-            node1.next = node2.next;
-            node1 = node1.next;
-            node2.next = node1.next;
-            node2 = node2.next;
+        //第三步：按升序顺序收集桶中其他数据
+        for (int i = 0; i < 1001; i++) {
+            while (bucket[i]-- > 0)
+                res[j++] = i;
         }
-        node1.next = pre2.next;
-        return pre.next;
-    }
-
-    public static ListNode oddEvenList1(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode pre = new ListNode(-1, head);//奇数pre
-        ListNode node1 = head;//奇数当前
-        ListNode node2 = head.next;//偶数当前
-        ListNode pre2 = new ListNode(-1, node2);//偶数pre
-        while (node1.next != null && node2.next != null) {
-            node1.next = node2.next;//隔一个偶后就是下一个奇
-            node1 = node1.next;//移动指针
-            node2.next = node1.next;//已经挪到一下的奇的后一个就是下一个偶
-            node2 = node2.next;//移动指针
-        }
-        node1.next = pre2.next;
-        return pre.next;
+        return res;
     }
 
 }
