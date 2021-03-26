adb783cfda61af3e080c35ebe72e73376831ad7c
machao <anshaym@163.com>
2021-03-26 11:29:41 +0800

    三数之和

---
+++ src/main/java/anshay/notebook/interview/double_pointer/Solution15.java
@@ -0,0 +1,50 @@
+package anshay.notebook.interview.double_pointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 15 三数之合
+ * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * @author machao
+ * @date 2021/2/28
+ */
+public class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            //最小的必须是非正数才可以得到和为0
+            if (nums[i] > 0) {
+                break;
+            } else if (i > 0 && nums[i] == nums[i - 1]) {
+                //去重
+                continue;
+            }
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    //去重,注意越界
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+}
