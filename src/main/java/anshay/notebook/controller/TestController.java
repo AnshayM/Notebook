b029ad0f274daf63611f965228237bcf863c71a8
anshay <anshay@foxmail.com>
2021-04-07 17:43:28 +0800

    修改测试

--- src/main/java/anshay/notebook/controller/TestController.java
+++ src/main/java/anshay/notebook/controller/TestController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author machao
  * @date 2020/10/14
  */
-// @RestController
-// @RequestMapping("/test")
 @RestController
 public class TestController {
     @ApiOperation("测试")
